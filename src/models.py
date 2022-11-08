9042a4522ea58f4609ab99bbe891c2e1b3eb00a0
MillanGarcia <millan.garcia.mata@gmail.com>
2022-11-08 10:37:36 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import datetime
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -14,17 +17,54 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    create_account = Column(DateTime, default=datetime.datetime.utcnow)
+    birth_year = Column(String(250), nullable=False)
+    profile_photo = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Followers(Base):
+    __tablename__ = 'followers'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    person_id_to = Column(Integer, ForeignKey('person.id'))
+    person_id_from  = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+class Posts(Base):
+    __tablename__ = 'posts'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
+
+class Multimedia(Base):
+    __tablename__ = 'multimedia'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    image_url = Column(String(250), nullable=True)
+    video_url = Column(String(250), nullable=True)
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+    posts = relationship(Posts)
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+    person = relationship(Person)
+    posts = relationship(Posts)
+
+
+
 
     def to_dict(self):
         return {}
